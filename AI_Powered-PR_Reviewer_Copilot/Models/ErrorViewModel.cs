ee12fff886b35e20edf1c2ffa89a20b22c9072d4
PremKumar A <premkumar.adaikalam@ibm.com>
2025-11-21 20:23:31 +0530

    Trigger PR for AI review

---
+++ AI_Powered-PR_Reviewer_Copilot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AI_Powered_PR_Reviewer_Copilot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
